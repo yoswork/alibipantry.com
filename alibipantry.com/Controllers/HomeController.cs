639363fe95714228cbe7f9baaadec532988e741b
Yos Noor <yusuf.noor@reed.co.uk>
2012-06-28 18:06:35 +0100

    Initial Commit
    There's a first time for everything

---
+++ alibipantry.com/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alibifood.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Cakes()
+        {
+            return View();
+        }
+
+        public ActionResult Preserves()
+        {
+            return View();
+        }
+    }
+}
